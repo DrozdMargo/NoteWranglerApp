5801fb76c1d3ee375831a3b89f11489f9cce46a3
Margo Drozd <drozdmargo@i.ua>
2016-12-12 20:21:04 +0200

    fixed services and index.html

--- app/javascript/controllers/notes-create-controller.js
+++ app/javascript/controllers/notes-create-controller.js
@@ -2,10 +2,7 @@ angular.module('NoteWrangler')
 	.controller('NotesCreateController', function($scope, Note, $location) {
 		$scope.note = new Note();
 		$scope.isSubmitting = false;
-		
-
-
-		$scope.saveNote = function (note) {
+			$scope.saveNote = function (note) {
 			$scope.isSubmitting = true;
 			note.$save().then(function() {
 				$location.path("/notes");
